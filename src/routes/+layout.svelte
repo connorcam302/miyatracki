dd78239fd1fa6bc11dab5c670c9afad115a17adb
connorcam302 <connorcam123@gmail.com>
2023-11-13 21:25:16 +0000

    work on navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
+	import Flame from '$lib/components/Flame.svelte';
 
 	export let data;
 
@@ -27,18 +28,53 @@
 <div class="bg-stone-700 min-h-screen relative">
 	{#if $page.url.pathname !== '/auth'}
 		<div class="bg-stone-800 px-2 h-10 text-lg text-stone-200 flex">
+			<div class="pr-2">
+				<Flame />
+			</div>
 			<div class="my-auto flex grow gap-3">
-				<span class="flex-none">miyatracki</span>
+				<button id class="mr-3 text-[#ffdc01] text-3xl" on:click={() => goto('/')}
+					>miyatracki</button
+				>
+				<button id="link" class="text-3xl" data-text="link1">link1</button>
+				<button id="link" class="text-3xl" data-text="link2">link2</button>
+				<button id="link" class="text-3xl" data-text="link3">link3</button>
 				<div class="flex-auto grow" />
-				{#if session}
-					<p>@{userData.displayName}</p>
-					<img src={userData.profilePicture} class="object-cover h-8 w-8" />
-					<button on:click={() => supabase.auth.signOut()}>sign out</button>
-				{:else}
-					<button on:click={() => goto('/auth')}>sign in</button>
-				{/if}
+				<div class="flex items-center gap-2">
+					{#if session}
+						<img src={userData.profilePicture} class="object-cover h-8 w-8" alt="profile" />
+						<button on:click={() => supabase.auth.signOut()}>sign out</button>
+					{:else}
+						<button on:click={() => goto('/auth')}>sign in</button>
+					{/if}
+				</div>
 			</div>
 		</div>
 	{/if}
 	<slot />
 </div>
+
+<style>
+	#link {
+		display: inline-block;
+		vertical-align: top;
+		position: relative;
+		line-height: 36px;
+		cursor: pointer;
+		color: rgb(240, 94, 27);
+	}
+
+	#link:before {
+		transition: height 0.5s ease-in-out;
+		content: attr(data-text);
+		position: absolute;
+		overflow: hidden;
+		color: #a8a29e;
+		height: 36px;
+		left: 0;
+		top: 0;
+	}
+
+	#link:hover:before {
+		height: 0;
+	}
+</style>
