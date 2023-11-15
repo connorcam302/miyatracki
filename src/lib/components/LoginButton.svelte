b62eb4415a5f11c665bb27b8841300534e92924e
connorcam302 <connorcam123@gmail.com>
2023-11-15 20:53:35 +0000

    sessions fixed and login page fixed

--- src/lib/components/LoginButton.svelte
+++ src/lib/components/LoginButton.svelte
@@ -4,29 +4,35 @@
 	const fill = new Map();
 	const border = new Map();
 	const font = new Map();
+	const click = new Map();
 
 	fill.set('google', '#FFFFFF');
 	border.set('google', '#747775');
 	font.set('google', '#1F1F1F');
+	click.set('google', '#EDEDED');
 
 	fill.set('discord', '#5865F2');
 	border.set('discord', '#5865F2');
 	font.set('discord', '#FFFFFF');
+	click.set('discord', '#4f5bdb');
 
 	fill.set('twitch', '#9146FF');
 	border.set('twitch', '#9146FF');
 	font.set('twitch', '#F0F0FF');
+	click.set('twitch', '#7d3cff');
 
 	fill.set('facebook', '#1877F2');
 	border.set('facebook', '#1877F2');
 	font.set('facebook', '#FFFFFF');
+	click.set('facebook', '#0f6ae6');
 
 	fill.set('twitter', '#000000');
 	border.set('twitter', '#000000');
 	font.set('twitter', '#FFFFFF');
+	click.set('twitter', '#000000');
 </script>
 
-<div
+<button
 	class={`border-2 rounded-2xl px-2 w-56 overflow-hidden h-11 flex items-center`}
 	style={`border-color: ${border.get(provider)};
             background-color: ${fill.get(provider)}; 
@@ -38,4 +44,4 @@
 			Sign in with {provider.charAt(0).toUpperCase() + provider.slice(1)}
 		</span>
 	</div>
-</div>
+</button>
