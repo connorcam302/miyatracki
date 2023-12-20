2e6089c0be73498166a0dec89a46de675ae6c2d9
Connor Campbell <connorcam123@gmail.com>
2023-12-20 15:51:57 +0000

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,3 @@
 <script lang="ts">
 	export let data;
 </script>
-
-{#if data.session}
-	<p>client-side data fetching with RLS</p>
-	<p>Hey, {data.session.user.id}</p>
-{:else}
-	<p>no session</p>
-{/if}
